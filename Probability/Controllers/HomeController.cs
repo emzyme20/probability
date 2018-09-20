c4e5a18890dd1be9bac608f07e54e58f204ce2ef
Emma Middlebrook <e.middlebrook@gmail.com>
2018-09-20 19:52:59 +0100

    Removed default actions from basic mvc website.

--- Probability/Controllers/HomeController.cs
+++ Probability/Controllers/HomeController.cs
@@ -41,22 +41,6 @@ namespace Probability.Controllers
             return View("Index", _mapper.Map<CalculatorViewModel>(model));
         }
         
-        [Route("about")]
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        [Route("contact")]
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         [Route("error")]
         public IActionResult Error()
         {
